affeaa5a2b4ad3171baccc99ca8d7fc613adc7e2
The_Slayer <brock.kerley@hotmail.com>
2022-08-14 15:24:55 +0930

    Initial 1.19.2

---
+++ src/main/java/net/jitl/core/init/JITL.java
@@ -0,0 +1,35 @@
+package net.jitl.core.init;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(JITL.MODID)
+public class JITL {
+    public static final String MODID = "jitl";
+    public static final String NAME = "Journey Into the Light";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    public JITL() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+
+        modEventBus.addListener(this::preInit);
+        modEventBus.addListener(this::clientSetup);
+        modEventBus.addListener(this::enqueue);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void preInit(final FMLCommonSetupEvent event) { }
+
+    private void clientSetup(final FMLClientSetupEvent event) { }
+
+    private void enqueue(InterModEnqueueEvent event) { }
+}
