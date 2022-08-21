36ab229334f0152057a527de9e38d24b18f72f64
The_Slayer <brock.kerley@hotmail.com>
2022-08-21 16:27:35 +0930

    Update OverlayRegistry.java

--- src/main/java/net/jitl/client/gui/OverlayRegistry.java
+++ src/main/java/net/jitl/client/gui/OverlayRegistry.java
@@ -2,14 +2,18 @@ package net.jitl.client.gui;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.jitl.client.gui.overlay.EssenceBar;
+import net.jitl.core.init.JITL;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.opengl.GL11;
 
+@Mod.EventBusSubscriber(modid = JITL.MODID)
 public class OverlayRegistry {
 
     @SubscribeEvent
     public static void registerOverlays(RegisterGuiOverlaysEvent event) {
+
         event.registerAboveAll("essence_bar", (gui, mStack, partialTicks, screenWidth, screenHeight) -> {
             RenderSystem.enableBlend();
             RenderSystem.blendFuncSeparate(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA, 1, 0);
