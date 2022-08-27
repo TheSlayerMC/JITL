1cc2c3d51386b67f67f73ac5cdd15dd3f04e3ae1
The_Slayer <brock.kerley@hotmail.com>
2022-08-27 11:48:23 +0930

    .

--- src/main/java/net/jitl/client/essence/PlayerEssence.java
+++ src/main/java/net/jitl/client/essence/PlayerEssence.java
@@ -16,38 +16,24 @@ public class PlayerEssence {
         return essence;
     }
 
-<<<<<<< HEAD
     public int getMaxEssence() {
         return maxEssence;
     }
 
-    public void setEssence(Player player, int value) {
-=======
     public void setEssence(int value) {
->>>>>>> parent of f289f8f (hmmmm)
         if(getEssence() != value) {
             essence = value;
         }
     }
 
-<<<<<<< HEAD
-    public void addEssence(Player player, int add) {
-        setEssence(player, Math.min(getEssence() + add, getMaxEssence()));
-=======
     public void addEssence(int add) {
         setEssence(Math.min(getEssence() + add, 11));
->>>>>>> parent of f289f8f (hmmmm)
     }
 
     public boolean consumeEssence(Player player, int price) {
         if(!player.isCreative()) {
             if(hasEssence(price)) {
-<<<<<<< HEAD
-                setEssence(player, getEssence() - price);
-                sendPacket(player);
-=======
                 setEssence(getEssence() - price);
->>>>>>> parent of f289f8f (hmmmm)
                 return true;
             }
             return false;
