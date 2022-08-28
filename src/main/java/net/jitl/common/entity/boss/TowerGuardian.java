168887c4cffea0749b8c1530170ae7f96c50e34d
The_Slayer <brock.kerley@hotmail.com>
2022-08-28 20:00:31 +0930

    Tower guardian animation fix

--- src/main/java/net/jitl/common/entity/boss/TowerGuardian.java
+++ src/main/java/net/jitl/common/entity/boss/TowerGuardian.java
@@ -6,9 +6,14 @@ import net.jitl.common.entity.base.JBossInfo;
 import net.jitl.common.entity.overworld.Floro;
 import net.jitl.common.entity.overworld.FloroAttackGoal;
 import net.jitl.core.init.JITL;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerBossEvent;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.BossEvent;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -20,6 +25,7 @@ import net.minecraft.world.entity.animal.Bee;
 import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.monster.RangedAttackMob;
+import net.minecraft.world.entity.monster.ZombifiedPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -36,22 +42,20 @@ public class TowerGuardian extends Monster implements RangedAttackMob, IAnimatab
     private final ServerBossEvent BOSS_INFO = new ServerBossEvent(this.getDisplayName(), BossEvent.BossBarColor.BLUE, BossEvent.BossBarOverlay.NOTCHED_6);
     private final BossBarRenderer BOSS_BAR = new BossBarRenderer(this, JITL.rl("textures/gui/bossbars/rockite_smasher.png"));
 
+    private static final EntityDataAccessor<Boolean> ATTACK = SynchedEntityData.defineId(TowerGuardian.class, EntityDataSerializers.BOOLEAN);
+
     public TowerGuardian(EntityType<? extends Monster> pEntityType, Level pLevel) {
         super(pEntityType, pLevel);
     }
 
     @Override
     protected void registerGoals() {
-        goalSelector.addGoal(3, new FloatGoal(this));//mutex 4
-        goalSelector.addGoal(4, new AvoidEntityGoal<>(this, Wolf.class, 6.0F, 1.0D, 1.2D));//mutex 1
-
-        goalSelector.addGoal(6, new WaterAvoidingRandomStrollGoal(this, 1.0D));//mutex 1
-        goalSelector.addGoal(7, new LookAtPlayerGoal(this, Player.class, 8.0F));//mutex 2
-        goalSelector.addGoal(7, new RandomLookAroundGoal(this));//mutex 3
-
-        targetSelector.addGoal(1, new HurtByTargetGoal(this));
-        targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, 5/*will target if rand.next(chance) == 0*/, true, false, null));
-        targetSelector.addGoal(3, new NearestAttackableTargetGoal<>(this, Bee.class, 5, true, false, null));
+        this.goalSelector.addGoal(0, new SmashingGoal(this, 1.0D, false)); //mutex 1
+        this.goalSelector.addGoal(8, new LookAtPlayerGoal(this, Player.class, 8.0F));
+        this.goalSelector.addGoal(8, new RandomLookAroundGoal(this));
+        this.goalSelector.addGoal(7, new WaterAvoidingRandomStrollGoal(this, 1.0D));
+        this.targetSelector.addGoal(1, (new HurtByTargetGoal(this)));
+        this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, Player.class, true));
     }
 
     public static AttributeSupplier createAttributes() {
@@ -89,17 +93,88 @@ public class TowerGuardian extends Monster implements RangedAttackMob, IAnimatab
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> event) {
-        if(event.isMoving()) {
+        if(event.isMoving() && !isAttacking()) {
             event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.tower_guardian.walk", true));
             return PlayState.CONTINUE;
         }
 
-        event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.tower_guardian.idle", true));
+        if(isAttacking()) {
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.tower_guardian.smash", true));
+            return PlayState.CONTINUE;
+        }
+
+        if(!isAttacking())
+            event.getController().setAnimation(new AnimationBuilder().addAnimation("animation.tower_guardian.idle", true));
         return PlayState.CONTINUE;
     }
 
     @Override
     public void performRangedAttack(LivingEntity pTarget, float pVelocity) {
 
+    }
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(ATTACK, false);
+    }
+
+    @Override
+    public void addAdditionalSaveData(CompoundTag compound) {
+        super.addAdditionalSaveData(compound);
+        compound.putBoolean("attack", isAttacking());
+    }
+
+    @Override
+    public void readAdditionalSaveData(CompoundTag compound) {
+        super.readAdditionalSaveData(compound);
+        if(compound.contains("attack")) {
+            setAttacking(compound.getBoolean("attack"));
+        }
+    }
+
+    public boolean isAttacking() {
+        return getEntityData().get(ATTACK);
+    }
+
+    public void setAttacking(boolean value) {
+        getEntityData().set(ATTACK, value);
+    }
+
+    private class SmashingGoal extends MeleeAttackGoal {
+        private final TowerGuardian entity;
+
+        private SmashingGoal(TowerGuardian entity, double speed, boolean useLongMemory) {
+            super(entity, speed, useLongMemory);
+            this.entity = entity;
+        }
+
+        @Override
+        public void start() {
+            super.start();
+        }
+
+        @Override
+        public void stop() {
+            super.stop();
+            this.entity.setAggressive(false);
+            setAttacking(false);
+        }
+
+        @Override
+        public boolean canUse() {
+            LivingEntity livingEntity = getTarget();
+            return livingEntity != null && livingEntity.isAlive();
+        }
+
+        @Override
+        protected void checkAndPerformAttack(LivingEntity enemy, double distToEnemySqr) {
+            double d0 = this.getAttackReachSqr(enemy);
+            if (distToEnemySqr <= d0) {
+                this.resetAttackCooldown();
+                setAttacking(true);
+                this.mob.doHurtTarget(enemy);
+            }
+        }
     }
 }
