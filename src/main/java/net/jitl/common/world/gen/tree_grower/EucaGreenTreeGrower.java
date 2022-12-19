792b17ff908c418d812ea6eaafaed4a62a3427c7
The_SlayerMC <brock.kerley@hotmail.com>
2022-12-19 14:48:23 +1030

    Revert "."
    This reverts commit b9e238b2178f1b2cfeb9671979242b129979d74c.

--- src/main/java/net/jitl/common/world/gen/tree_grower/EucaGreenTreeGrower.java
+++ src/main/java/net/jitl/common/world/gen/tree_grower/EucaGreenTreeGrower.java
@@ -1,6 +1,5 @@
 package net.jitl.common.world.gen.tree_grower;
 
-import net.jitl.common.world.gen.JConfiguredFeatures;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.block.grower.AbstractTreeGrower;
@@ -13,6 +12,6 @@ public class EucaGreenTreeGrower extends AbstractTreeGrower {
     @Nullable
     @Override
     protected ResourceKey<ConfiguredFeature<?, ?>> getConfiguredFeature(@NotNull RandomSource random, boolean largeHive) {
-        return JConfiguredFeatures.EUCA_GREEN_TREE;
+        return null;//JConfiguredFeatures.EUCA_GREEN_TREE;
     }
 }
