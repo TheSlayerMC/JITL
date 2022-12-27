fc8f3e826d2aae38564e3a858b8f08a45191d36b
The_SlayerMC <brock.kerley@hotmail.com>
2022-12-27 20:10:49 +1030

    forge 44.0.40

--- src/main/java/net/jitl/client/render/block/RockiteSpawnerRenderer.java
+++ src/main/java/net/jitl/client/render/block/RockiteSpawnerRenderer.java
@@ -1,5 +1,7 @@
 package net.jitl.client.render.block;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.jitl.client.model.block.RockiteSpawnerModel;
 import net.jitl.common.block.entity.RockiteSpawnerEntity;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -7,6 +9,7 @@ import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class RockiteSpawnerRenderer extends GeoBlockRenderer<RockiteSpawnerEntity> {
@@ -17,8 +20,14 @@ public class RockiteSpawnerRenderer extends GeoBlockRenderer<RockiteSpawnerEntit
 
     @Override
     public RenderType getRenderType(RockiteSpawnerEntity animatable, ResourceLocation texture, @Nullable MultiBufferSource bufferSource, float partialTick) {
-        float size = 2.0F;
-        //stack.scale(size, size, size);TODO
         return RenderType.entityTranslucent(getTextureLocation(animatable));
+    }
+
+    @Override
+    public void preRender(PoseStack poseStack, RockiteSpawnerEntity animatable, BakedGeoModel model, MultiBufferSource bufferSource, VertexConsumer buffer, boolean isReRender, float partialTick, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
+        float size = 2.0F;
+        poseStack.scale(size, size, size);
+        poseStack.translate(-0.5F, 0, -0.5F);
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, red, green, blue, alpha);
     }
 }
