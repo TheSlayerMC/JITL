7ca72d9f1c5bc31542a0b472f4fa64bf6f7aad23
Dizzlepop12 <foolishnessisnecessary@gmail.com>
2023-03-12 21:34:36 -0700

    Corba swamp shelf started

--- src/main/java/net/jitl/common/world/gen/JFoliagePlacers.java
+++ src/main/java/net/jitl/common/world/gen/JFoliagePlacers.java
@@ -1,6 +1,7 @@
 package net.jitl.common.world.gen;
 
-import net.jitl.common.world.gen.tree_grower.SphericalFoliagePlacer;
+import net.jitl.common.world.gen.tree_grower.foliageplacers.SphericalFoliagePlacer;
+import net.jitl.common.world.gen.tree_grower.foliageplacers.SwampFoliagePlacer;
 import net.jitl.core.init.JITL;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,4 +13,5 @@ public class JFoliagePlacers {
     public static final DeferredRegister<FoliagePlacerType<?>> REGISTRY = DeferredRegister.create(ForgeRegistries.FOLIAGE_PLACER_TYPES, JITL.MODID);
 
     public static final RegistryObject<FoliagePlacerType<SphericalFoliagePlacer>> SPHERICAL_FOLIAGE_PLACER = REGISTRY.register("spherical_foliage_placer", () -> new FoliagePlacerType<>(SphericalFoliagePlacer.CODEC));
+    public static final RegistryObject<FoliagePlacerType<SwampFoliagePlacer>> SWAMP_FOLIAGE_PLACER = REGISTRY.register("swamp_foliage_placer", () -> new FoliagePlacerType<>(SwampFoliagePlacer.CODEC));
 }
---
+++ src/main/java/net/jitl/common/world/gen/tree_grower/foliageplacers/SphericalFoliagePlacer.java
@@ -0,0 +1,76 @@
+package net.jitl.common.world.gen.tree_grower.foliageplacers;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.jitl.common.world.gen.JFoliagePlacers;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.util.RandomSource;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.world.level.LevelSimulatedReader;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.FoliagePlacerType;
+import org.jetbrains.annotations.NotNull;
+import java.util.function.BiConsumer;
+
+public class SphericalFoliagePlacer extends BlobFoliagePlacer {
+
+    public static final Codec<SphericalFoliagePlacer> CODEC = RecordCodecBuilder.create((instance) -> blobParts(instance).apply(instance, SphericalFoliagePlacer::new));
+
+    public SphericalFoliagePlacer(IntProvider featureSpread, IntProvider featureSpread1, int i) {
+        super(featureSpread, featureSpread1, i);
+    }
+
+    @Override
+    protected @NotNull FoliagePlacerType<?> type() {
+        return JFoliagePlacers.SPHERICAL_FOLIAGE_PLACER.get();
+    }
+
+    @Override
+    protected void createFoliage(@NotNull LevelSimulatedReader reader, @NotNull BiConsumer<BlockPos, BlockState> blockSetter, @NotNull RandomSource rand, @NotNull TreeConfiguration baseTreeFeatureConfig, int maxFreeTreeHeight, FoliageAttachment foliage, int foliageHeight, int foliageRadius, int offset) {
+        int size = foliageRadius + foliage.radiusOffset();
+        BlockPos pos = foliage.pos().above(offset);
+        pos = pos.offset(Direction.UP.getNormal());
+        for(byte x = 0; x <= size; x++) {
+            for(byte y = 0; y <= size; y++) {
+                for(byte z = 0; z <= size; z++) {
+
+                    int distance;
+                    if(x >= y & x >= z) {
+                        distance = x + (Math.max(y, z) >> 1) + (Math.min(y, z) >> 1);
+                    } else if(y >= x & y >= z) {
+                        distance = y + (Math.max(x, z) >> 1) + (Math.min(x, z) >> 1);
+                    } else {
+                        distance = z + (Math.max(x, y) >> 1) + (Math.min(x, y) >> 1);
+                    }
+                    if (distance <= size) {
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(+x, +y, +z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(+x, +y, -z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(-x, +y, +z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(-x, +y, -z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(+x, -y, +z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(+x, -y, -z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(-x, -y, +z), foliageHeight, offset, false);
+                        placeLeavesRow(reader, blockSetter, rand, baseTreeFeatureConfig, pos.offset(-x, -y, -z), foliageHeight, offset, false);
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public int foliageHeight(@NotNull RandomSource random, int height, @NotNull TreeConfiguration baseTreeFeatureConfig) {
+        return 0;
+    }
+
+    @Override
+    protected boolean shouldSkipLocation(@NotNull RandomSource random, int localX, int localY, int localZ, int range, boolean large) {
+        if (localY == 0) {
+            return (localX > 1 || localZ > 1) && localX != 0 && localZ != 0;
+        } else {
+            return localX == range && localZ == range && range > 0;
+        }
+    }
+}
