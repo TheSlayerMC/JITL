1394d76dbf18768e31063f3ecb685a9da0461293
The_Slayer <brock.kerley@hotmail.com>
2023-03-18 11:23:33 +1030

    clouds in boil

--- src/main/java/net/jitl/client/render/world/BoilRenderInfo.java
+++ src/main/java/net/jitl/client/render/world/BoilRenderInfo.java
@@ -6,7 +6,6 @@ import com.mojang.blaze3d.vertex.*;
 import com.mojang.math.Axis;
 import net.jitl.core.init.JITL;
 import net.minecraft.client.Camera;
-import net.minecraft.client.CloudStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.renderer.*;
@@ -39,6 +38,12 @@ public class BoilRenderInfo extends DimensionSpecialEffects {
     }
 
     @Override
+    public boolean renderClouds(ClientLevel level, int ticks, float partialTick, PoseStack poseStack, double camX, double camY, double camZ, Matrix4f projectionMatrix) {
+        new BoilCloudRenderer().render(level, ticks, partialTick, poseStack, Minecraft.getInstance(), camX, camY, camZ, projectionMatrix);
+        return true;
+    }
+
+    @Override
     public @NotNull Vec3 getBrightnessDependentFogColor(Vec3 vector3d, float float_) {
         float color = 0.95F + 0.05F;
         return vector3d.multiply((float_ * color), (float_ * color), (float_ * color));
@@ -64,6 +69,7 @@ public class BoilRenderInfo extends DimensionSpecialEffects {
                 BufferBuilder bufferbuilder = Tesselator.getInstance().getBuilder();
                 RenderSystem.depthMask(false);
                 RenderSystem.setShaderColor(f, f1, f2, 1.0F);
+                assert this.skyBuffer != null;
                 this.skyBuffer.bind();
                 VertexBuffer.unbind();
                 RenderSystem.enableBlend();
