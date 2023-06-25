e3aa134c0e445a814662c4f86d4b8f21ec5c13d3
The_Slayer <brock.kerley@hotmail.com>
2023-06-25 17:47:09 +0930

    Update ChatUtils.java

--- src/main/java/net/jitl/client/ChatUtils.java
+++ src/main/java/net/jitl/client/ChatUtils.java
@@ -1,5 +1,6 @@
 package net.jitl.client;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
 
@@ -13,5 +14,21 @@ public class ChatUtils {
     public static void addChatBarChat(Player player, String chat) {
         if(!player.level().isClientSide)
             player.displayClientMessage(Component.translatable(chat), false);
+    }
+
+    public static void addColouredDialogStyleChat(Player player, ChatFormatting c, String chat) {
+        Component comp = Component.translatable(chat);
+        comp.getStyle().withColor(c);
+        if(!player.level().isClientSide) {
+            player.displayClientMessage(comp, true);
+        }
+    }
+
+    public static void addColouredChatBarChat(Player player, ChatFormatting c, String chat) {
+        Component comp = Component.translatable(chat);
+        comp.getStyle().withColor(c);
+        if(!player.level().isClientSide) {
+            player.displayClientMessage(comp, false);
+        }
     }
 }
